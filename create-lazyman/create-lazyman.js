48560d6295b5a347f2a4cef01e607a1c2aa6b768
Rohit Vaswani <rohit.vaswani@fancode.com>
2022-01-25 00:30:39 +0530

    feat(problem): Create lazyManC

---
+++ create-lazyman/create-lazyman.js
@@ -0,0 +1,115 @@
+/*
+        Source: https://bigfrontend.dev/problem/create-lazyman (130_BFE)
+        Solution link: https://bigfrontend.dev/problem/create-lazyman/discuss/472
+        Difficulty: TODO_DI_MEDIUM
+        Algorithm: ?
+        Company: ?
+        DataStructure: ?
+        TODO_STANDARD_PROBLEM: NO
+        TODO_MUST_SOLVE: YES
+        TODO_INTERESTING: NO
+        TODO_QUESTIONS:
+            a)
+        TODO_LEARNING:
+            a)
+        TODO_REMEMBER:
+            a) Using setTimeOut, you ensured all the actions are queued before the exection starts
+        TODO_SOLUTION:
+            a) Define actionTypes and actionHandlers against case.
+            b) Queue the actions into normal and urgent queue.
+            c) Process them one by one
+*/
+
+class LazyMan {
+
+    constructor(name, loggerFn) {
+        this.name = name
+        this.logger = loggerFn
+        this.actionTypes = {
+            greet: 'greet',
+            eat: 'eat',
+            sleep: 'sleep'
+        }
+
+
+        this.normalTask = []
+        this.urgentTask = []
+        this.greet(name)
+
+        setTimeout(() => {
+            this.handleAction()
+        }, 0)
+
+    }
+
+
+    greet(name) {
+        this.normalTask.push([this.actionTypes.greet, name])
+        return this
+    }
+
+    eat(product) {
+        this.normalTask.push([this.actionTypes.eat, product])
+        return this
+    }
+
+    sleep(timeInterval) {
+        this.normalTask.push([this.actionTypes.sleep, timeInterval * 1000])
+        return this
+    }
+
+    sleepFirst(timeInterval) {
+        this.urgentTask.push([this.actionTypes.sleep, timeInterval * 1000])
+        return this
+    }
+
+    log(val) {
+        this.logger(val)
+    }
+
+    handleAction() {
+
+        let task;
+
+        task = this.urgentTask.shift()
+
+        if(!task) {
+            task = this.normalTask.shift()
+        }
+
+        if(!task) {
+            return null
+        }
+
+        const [action, params] = task
+
+        switch (action) {
+            case this.actionTypes.greet:
+                this.log(`Hi, I'm ${params}`)
+                this.handleAction()
+                break;
+            case this.actionTypes.eat:
+                this.log(`Eat ${params}`)
+                this.handleAction()
+                break
+            case this.actionTypes.sleep:
+                setTimeout(() => {
+                    this.log(`Wake up after ${params}`)
+                    this.handleAction()
+                }, params)
+                break
+            default:
+                break
+        }
+
+    }
+
+
+}
+
+// const inst = new LazyMan('Jack', console.log)
+// new LazyMan('Jack', console.log).eat('banana').eat('apple')
+// new LazyMan('Jack', console.log).eat('banana').sleep(3).eat('apple').sleep(1)
+new LazyMan('Jack', console.log).eat('banana').sleepFirst(2).eat('apple').sleep(1)
+
+
