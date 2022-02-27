a2af99d5dae30020871b84edc4011e836c882f12
Rohit Vaswani <rohit.vaswani@fancode.com>
2022-02-28 00:48:15 +0530

    feat(problems): Solved Event Delegation

---
+++ event-delegation/event-delegation.js
@@ -0,0 +1,81 @@
+/*
+        Source: https://bigfrontend.dev/problem/event-delegation
+        Solution link: https://bigfrontend.dev/problem/event-delegation/discuss/9066
+        Difficulty: TODO_DI_MEDIUM
+        Algorithm: ?
+        Company: ?
+        DataStructure: ?
+        TODO_STANDARD_PROBLEM: NO
+        TODO_MUST_SOLVE: NO
+        TODO_MEMORIZE: NO
+        TODO_INTERESTING: NO
+        TODO_QUESTIONS:
+            a)
+        TODO_LEARNING:
+            a)
+        TODO_REMEMBER:
+            a) Callback of an event gives event object containing {target, parentElement, tagName}
+            b) Stop propagation and stopImmediate propagation is registered on the event object.
+        TODO_SOLUTION:
+            a) Register an event on the rootNode.
+            b) Maintain the map of key as rootNode and eventhandlers as value of the Map.
+            c) When an event happens, iterate over the event target and it's parentElement. process all the registered event handlers whose predicate is true.
+
+*/
+
+
+let eventHandlersMap = new Map()
+
+function onClick(rootNode, predicate, eventHandler) {
+
+    if (eventHandlersMap.has(rootNode)) {
+        eventHandlersMap.get(rootNode).push({eventHandler, predicate})
+        return eventHandlersMap.get(rootNode)
+    }
+
+
+    let stopPropagation = false
+    let stopImmediatePropagation = false
+
+    const runEvents = (element, event, eventHandlers) => {
+
+        for (let {predicate, eventHandler} of eventHandlers) {
+            if (predicate(element)) {
+                eventHandler.call(element, event)
+                if (stopImmediatePropagation) break;
+            }
+        }
+
+    }
+
+    eventHandlersMap.set(rootNode, [{eventHandler, predicate}])
+
+
+    rootNode.addEventListener('click', (event) => {
+
+        let eventHandlers = eventHandlersMap.get(rootNode)
+
+        event.stopPropagation = () => {
+            stopPropagation = true
+        }
+
+        event.stopImmediatePropagation = () => {
+            stopImmediatePropagation = true
+        }
+
+        let targetElem = event.target
+        while ((targetElem || targetElem === rootNode) && !stopPropagation) {
+            runEvents(targetElem, event, eventHandlers)
+            targetElem = targetElem.parentElement
+        }
+    })
+}
+
+
+window.addEventListener('DOMContentLoaded', (event) => {
+
+    console.log('event')
+    onClick(document.getElementById("parent1"), el => el.tagName.toLowerCase() === "div", function(event) {
+        console.log('EVENT DETAILS', this)
+    })
+});
