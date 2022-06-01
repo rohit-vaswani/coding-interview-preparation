b346546bd4a6e5cb72d4b01f7a9ec32312047bfa
Rohit Vaswani <rohit.vaswani@fancode.com>
2022-06-02 00:16:50 +0530

    feat(movies): Imbalanced nodes

--- SubSet/subset.js
+++ SubSet/subset.js
@@ -1,11 +1,12 @@
 /*
     TODO_PENDING: https://leetcode.com/problems/combination-sum/discuss/16502/A-general-approach-to-backtracking-questions-in-Java-(Subsets-Permutations-Combination-Sum-Palindrome-Partitioning)
-    Ref: https://stackoverflow.com/questions/42773836/how-to-find-all-subsets-of-a-set-in-javascript    
+    Ref: https://stackoverflow.com/questions/42773836/how-to-find-all-subsets-of-a-set-in-javascript
     Problem statement: https://leetcode.com/problems/subsets/
 
 */
 
 
+// TODO_REMEMBER: TO find all the subset combination of the given array.
 const getAllSubsets =  theArray => theArray.reduce( (subsets, value) => {
     return subsets.concat(subsets.map(set => [value, ...set]))
 }, [[]] );
